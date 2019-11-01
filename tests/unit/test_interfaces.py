0fe87ba816d74aba1f9802ec5ca337f8fd766f6c
Farley, Tom <tom.farley@ukaea.uk>
2019-11-01 09:02:15 +0000

    Test pass_id string

--- tests/unit/test_interfaces.py
+++ tests/unit/test_interfaces.py
@@ -24,24 +24,24 @@ class TestInterfaces(unittest.TestCase):
         self.assertEqual(out, user_defaults)
 
     def test_id_strings(self):
-        values = {'machine': 'MAST', 'camera': 'rit', 'pulse': 23586, 'lens': '25mm', 't_int': 2.7e-5,
+        values = {'machine': 'MAST', 'camera': 'rit', 'pass_no': 1, 'pulse': 23586, 'lens': '25mm', 't_int': 2.7e-5,
                   'frame_no': 1234, 'frame_time': 0.5123}
 
         id_strings = {}
-        kwargs = {key: values[key] for key in ['pulse', 'camera', 'machine']}
+        kwargs = {key: values[key] for key in ['pulse', 'camera', 'machine', 'pass_no']}
         id_strings = generate_pulse_id_strings(id_strings, **kwargs)
-        expected = {'pulse_id': 'MAST-23586', 'camera_id': 'MAST-23586-rit'}
+        expected = {'pulse_id': 'MAST-23586', 'camera_id': 'MAST-23586-rit', 'pass_id': 'MAST-23586-1'}
         self.assertDictEqual(id_strings, expected)
 
         kwargs = {key: values[key] for key in ['lens', 't_int']}
         id_strings = generate_camera_id_strings(id_strings, **kwargs)
-        expected = {'pulse_id': 'MAST-23586', 'camera_id': 'MAST-23586-rit',
+        expected = {'pulse_id': 'MAST-23586', 'camera_id': 'MAST-23586-rit', 'pass_id': 'MAST-23586-1',
                     'lens_id': 'MAST-23586-rit-25mm', 't_int_id': 'MAST-23586-rit-25mm-2.7e-05'}
         self.assertDictEqual(id_strings, expected)
 
         kwargs = {key: values[key] for key in ['frame_no', 'frame_time']}
         id_strings = generate_frame_id_strings(id_strings, **kwargs)
-        expected = {'pulse_id': 'MAST-23586', 'camera_id': 'MAST-23586-rit',
+        expected = {'pulse_id': 'MAST-23586', 'camera_id': 'MAST-23586-rit', 'pass_id': 'MAST-23586-1',
                     'lens_id': 'MAST-23586-rit-25mm', 't_int_id': 'MAST-23586-rit-25mm-2.7e-05',
                     'frame_id': 'MAST-23586-rit-1234', 'time_id': 'MAST-23586-0.5123'}
         self.assertDictEqual(id_strings, expected)
