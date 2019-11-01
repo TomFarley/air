72baf009a83c9ab266f5cdf23da2edd3d6583d00
Farley, Tom <tom.farley@ukaea.uk>
2019-11-01 11:10:52 +0000

    Define die() and switch from cmake3

--- dependencies/make-uda-client-only.sh
+++ dependencies/make-uda-client-only.sh
@@ -7,6 +7,9 @@ UDA_INSTALL_DIR=$PWD/uda-install-2.2.6
 # SWIG location, needed on Freia but not if swig is system installed
 SWIG_DIR=/common/projects/UDA/swig/bin/swig
 
+# Define die function
+die() { echo “$*” 1>&2; exit 1; }
+
 # Clone UDA repo
 git clone https://oauth2:$AUTH_TOKEN@git.ccfe.ac.uk/MAST-U/UDA.git -b $UDA_TAG $UDA_RELEASE_DIR || die "failed to clone
 UDA repo"
@@ -17,8 +20,8 @@ CC=gcc CXX=g++ cmake3 -Bbuild -H. -DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=MAST \
    -DCMAKE_INSTALL_PREFIX=$UDA_INSTALL_DIR \
    -DCLIENT_ONLY=TRUE \
    -DSWIG_EXECUTABLE=$SWIG_DIR || die "failed to configure UDA"
-cmake3 --build build/ || die "failed to build UDA"
-cmake3 --build build/ --target install || die "failed to install UDA"
+cmake --build build/ || die "failed to build UDA"
+cmake --build build/ --target install || die "failed to install UDA"
 
 
 
