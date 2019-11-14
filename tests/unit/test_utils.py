07bffca4d9a6bbf79857af47274f3e1ddfcd815d
Farley, Tom <tom.farley@ukaea.uk>
2019-11-14 14:59:41 +0000

    Add test_locate_file, test_make_iterable,

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import numpy as np
 
-from fire.utils import update_call_args
+from fire.utils import update_call_args, locate_file, make_iterable
 
 pwd = Path(__file__).parent
 
@@ -28,6 +28,80 @@ class TestUtils(unittest.TestCase):
         expected = (30378, 'rit', 'MAST_U')
         self.assertEqual(outputs, expected)
 
+        inputs = (None, None, None)
+        outputs = update_call_args(user_defaults, *inputs)
+        expected = (30378, 'rir', 'MAST_U')
+        self.assertEqual(outputs, expected)
+
+    def test_locate_file(self):
+        file = Path(__file__)
+        paths = ['/some/{special}/path/', file.parent, '~']
+        fns = ['incorrect_name.ext', file.name, '{var}_name.txt']
+        path_kws = {'special': 'wonderful'}
+        fn_kws = {'var': 'my'}
+
+        p, f = locate_file(paths, fns, path_kws=path_kws, fn_kws=fn_kws, return_raw_fn=False, return_raw_path=False)
+        self.assertEqual(p, paths[1])
+        self.assertEqual(f, fns[1])
+
+        # TODO: Test returning raw for subbed args
+
+    def test_make_iterable(self):
+        # Scalars - nest
+        input = 3
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        self.assertEqual(output, [input])
+        input = 'hello'
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        self.assertEqual(output, [input])
+        input = 43653.547
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        self.assertEqual(output, [input])
+        input = None
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        self.assertEqual(output, [input])
+        input = Path('~/path')
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        self.assertEqual(output, [input])
+
+        # Already iterables
+        input = (1, 4.6, 'str')
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        self.assertEqual(output, input)
+        input = np.linspace(0, 10, 11)
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        np.testing.assert_equal(output, input)
+        input = ['hello', 'bye']
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        self.assertEqual(output, input)
+        input = {'hello': 'bye'}
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=None)
+        self.assertEqual(output, input)
+
+        # To ndarray
+        input = [1, 7, 8.5, 13, 2]
+        output = make_iterable(input, ndarray=True, cast_to=None, cast_dict=None, nest_types=None)
+        np.testing.assert_equal(output, np.array(input))
+        input = 3
+        output = make_iterable(input, ndarray=True, cast_to=None, cast_dict=None, nest_types=None)
+        np.testing.assert_equal(output, np.array([input]))
+
+        # cast_to
+        input = [1, 7, 8.5, 13, 2]
+        output = make_iterable(input, ndarray=False, cast_to=tuple, cast_dict=None, nest_types=None)
+        self.assertEqual(output, tuple(input))
+
+        # cast_dict
+        input = [1, 7, 8.5, 13, 2]
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict={list:tuple}, nest_types=None)
+        self.assertEqual(output, tuple(input))
+
+        # nest_types
+        input = {'hello': 'bye'}
+        output = make_iterable(input, ndarray=False, cast_to=None, cast_dict=None, nest_types=(dict,))
+        self.assertEqual(output, [input])
+
+
 def suite():
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
@@ -38,5 +112,5 @@ def suite():
 if __name__ == '__main__':
 
     runner = unittest.TextTestRunner(failfast=True)
-    # unittest.main()
-    runner.run(suite())
+    unittest.main()
+    # runner.run(suite())
