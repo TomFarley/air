35d70bb6bac98323ba0888a8eb050fd51b16fa9a
Farley, Tom <tom.farley@ukaea.uk>
2019-11-14 13:24:29 +0000

    Rename file, add docstring and move to /movie_pulgins/

--- fire/interfaces/movie_plugins/movie_plugin_template.py
+++ fire/interfaces/movie_plugins/movie_plugin_template.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-"""
+"""Template for new movie pluglin for use with FIRE IR analysis code
 
-
-Created: 
+1) Add code for your movie format to the two functions below.
+2) Update "movie_plugin_name" variable (this must be unique)
+3) Update desciription and add any other informations fields to the "plugin_info" variable
+4) Update your .fire_config.json file to include this file in the "paths_input/movie_plugins"
+5) Update your .fire_config.json file to include the name of your plugin under the relevent camera description
+    "machines"/<my_machine>/"cameras"/<my_camera>/"movie_plugins"/[<movie_plugin_name>, ...]
 """
 
 import logging
-from typing import Union, Iterable, Tuple, Optional
+from typing import Union, Iterable, Tuple, Optional, Dict, Any
 from pathlib import Path
 
 import numpy as np
@@ -15,8 +19,11 @@ import numpy as np
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+movie_plugin_name = '<my_plugin>'
+plugin_info = {'description': 'This plugin reads <my format> format movie files'}
+
 def read_movie_meta(pulse: int, camera: str, n_start:Optional[int]=None, n_end:Optional[int]=None,
-                    stride:Optional[int]=1):
+                    stride:Optional[int]=1) -> Dict[str, Any]:
     """Return UDA movie object for given pulse, camera and frame range
 
     :param pulse: MAST-U pulse number
@@ -34,7 +41,8 @@ def read_movie_meta(pulse: int, camera: str, n_start:Optional[int]=None, n_end:O
     return movie_meta
 
 def read_movie_data(pulse: int, camera: str, n_start:Optional[int]=None, n_end:Optional[int]=None,
-                    stride:Optional[int]=1, transforms: Iterable[str]=None):
+                    stride:Optional[int]=1, transforms: Iterable[str]=None) \
+                    -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Return UDA movie object for given pulse, camera and frame range
 
     :param pulse: MAST-U pulse number
