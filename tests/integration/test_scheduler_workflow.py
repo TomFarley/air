f56ed12de5f3d73f3d7e820c3ca22388ed89e268
Farley, Tom <tom.farley@ukaea.uk>
2019-12-06 16:24:37 +0000

    Change pulse and camera for integration test

--- tests/integration/test_scheduler_workflow.py
+++ tests/integration/test_scheduler_workflow.py
@@ -11,7 +11,7 @@ pwd = Path(__file__).parent
 class TestSchedulerWorkflow(unittest.TestCase):
 
     def test_scheduler_workflow(self):
-        inputs = {'pulse': 30378, 'camera': 'rit', 'machine': 'MAST', 'pass_no': 0, 'scheduler': False,
+        inputs = {'pulse': 23586, 'camera': 'rir', 'machine': 'MAST', 'pass_no': 0, 'scheduler': False,
                   'magnetics': False}
         scheduler_workflow(**inputs)
 
