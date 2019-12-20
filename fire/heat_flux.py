50eadc6d0fda7e419c2c0246d79b6a7d3fb22a52
Farley, Tom <tom.farley@ukaea.uk>
2019-12-20 16:42:21 +0000

    Pass material properties to theodor

--- fire/heat_flux.py
+++ fire/heat_flux.py
@@ -20,28 +20,46 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def calc_heatflux(t, temperatures, s_analsis_path, tile_properties):
+def calc_heatflux(t, temperatures, analysis_path, material_properties):
     """"""
     import theodor
-    d_target
-    alpha_bot = alphas['tile_bottom']
-    alpha_top = alphas['tile_surface']
-
-    d_target=0.040
-    diff = np.array([ 70.63, 48.25, 37.78])*1.0e-6
-    lam = np.array([174.9274, 133.1148, 110.4595])
-    #diff = np.array([240.87, 61.53, 34.86])*1e-6    # mm^2/s    Valerias JET data, 01/12/2008
-    #lam  = np.array([305.28,175.68,117.12])         # W/m/K     Valerias JET data
-    aniso=1.00
-    alpha_bot=200.0
-    acl=alphavector if alphavector is not None else np.array([1.])
-    alpha_top = alpha*acl if alpha is not None else 220000.0*acl
 
     # TODO: Check time axis is uniform
+    dt = np.diff(t)
+    if not np.all(np.isclose(dt, dt[0], rtol=1e-3)):
+        raise ValueError(f'Time axis of data supplied to THEODOR is not uniform. dt: {set(dt)}')
+    # TODO: Check analysis path for jumps/tile gaps etc
+
+    # TODO generalise to other path names (i.e. other than 'path')
+    material_ids = set(np.array(analysis_path['material_id_path']))
+    if len(material_ids) > 1:
+        raise NotImplementedError
+    # TODO: Loop over sections of path with different material properties or tile gaps etc
+    xpix_path, ypix_path = analysis_path['x_pix_path'], analysis_path['y_pix_path']
+    temperature_path = temperatures[:, ypix_path, xpix_path]
+    s_path = analysis_path['s_path']  # spatial coordinate along tile surface
+    theo_kwargs = material_properties[list(material_ids)[0]]
+
+    # alpha_bot = alphas['tile_bottom']
+    # alpha_top = alphas['tile_surface']
+    #
+    # d_target=0.040
+    # diff = np.array([ 70.63, 48.25, 37.78])*1.0e-6
+    # lam = np.array([174.9274, 133.1148, 110.4595])
+    # #diff = np.array([240.87, 61.53, 34.86])*1e-6    # mm^2/s    Valerias JET data, 01/12/2008
+    # #lam  = np.array([305.28,175.68,117.12])         # W/m/K     Valerias JET data
+    # aniso=1.00
+    # alpha_bot=200.0
+    # acl=alphavector if alphavector is not None else np.array([1.])
+    # alpha_top = alpha*acl if alpha is not None else 220000.0*acl
+
+
     # For hints as to meanings to theodor arguments see:
     # https://users.euro-fusion.org/openwiki/index.php/THEODOR#theo_mul_33.28.29
-    qtile, extra_results = theodor.theo_mul_33(temperatures, t, s_analsis_path, d_target, alpha_bot, alpha_top, diff, lam,
-                                               aniso, x_Tb=x_Tb, y_Tb=y_Tb, test=True, verbose=verbose)
+    heat_flux, extra_results = theodor.theo_mul_33(temperature_path, t, s_path, test=True, verbose=True, **theo_kwargs)
+    #               d_target, alpha_bot, alpha_top, diff, lam, aniso, x_Tb=x_Tb, y_Tb=y_Tb,
+
+    return heat_flux, extra_results
 
 if __name__ == '__main__':
     pass
