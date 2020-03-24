4173d87c266575d1f15e978f25e81a879772e107
Farley, Tom <tom.farley@ukaea.uk>
2020-03-24 14:43:12 +0000

    Copy netcdf fields from old IDL code

--- fire/interfaces/ouput_data_plugins/output_netcdf.py
+++ fire/interfaces/ouput_data_plugins/output_netcdf.py
@@ -125,6 +125,162 @@ def write_processed_ir_to_netcdf():
  ListData(shot=0, pass_=-1, signal_name='AIR_PHI_EXTENT', generic_name='', source_alias='air', type='Analysed', description='Replaced', signal_status=1, mds_name='\\TOP.ANALYSED.AIR:PHI_EXTENT'),
  ListData(shot=0, pass_=-1, signal_name='AIR_PHI_EXTENT_ISP', generic_name='', source_alias='air', type='Analysed', description='Toroidal extent of analysis path inner strike point', signal_status=1, mds_name='\\TOP.ANALYSED.AIR.PHI_EXTENT_:ISP'),
  ListData(shot=0, pass_=-1, signal_name='AIR_PHI_EXTENT_OSP', generic_name='', source_alias='air', type='Analysed', description='Toroidal extent of analysis path outer strike point', signal_status=1, mds_name='\\TOP.ANALYSED.AIR.PHI_EXTENT_:OSP'),
+    
+    
+    NETCDF output code from /home/athorn/IR/Latest/sched_air_netcdf/src/netcdfout.pro:
+    	;write out the coordinates
+	nt=n_elements(tout)
+	nrv=n_elements(rout_norm)
+
+	;dimensions
+	rc=putdata(nt, stepId='dimension', group='/'+prefix, name='time')
+	rc=putdata(nrv, stepId='dimension', group='/'+prefix, name='radius')
+	rc=putdata(1, stepId='dimension', group='/'+prefix, name='value')
+	;coordinates
+	rc=putdata(tout, stepId='coordinate', group='/'+prefix, name='time', $
+		units='t', label='Time', class='time')
+	rc=putdata(rout_norm, stepId='coordinate', $
+				group='/'+prefix, name='radius', $
+				units='t', label='Time')
+	rc=putdata([0], stepId='coordinate', group='/'+prefix, name='value', $
+		units='', label='Value')
+
+	;write out the data
+	rc=putdata(qmin(sti:eti), stepid='variable', $
+		group='/'+prefix, name='minpower_density', $
+		dimensions='time', units='MW/m^2', label='Min q')
+
+	rc=putdata(qmax(sti:eti), stepid='variable', $
+		group='/'+prefix, name='pkpower_density', $
+		dimensions='time', units='MW/m^2', label='Max q')
+;
+   if (nalpha gt 1) then begin
+		rc=putdata(qmin_ELM(sti:eti), stepid='variable', $
+			group='/'+prefix, name='minpower_density_elm', $
+			dimensions='time', units='MW/m^2', label='Min q (ELM alpha)')
+
+		rc=putdata(qmax_ELM(sti:eti), stepid='variable', $
+			group='/'+prefix, name='pkpower_density_elm', $
+			dimensions='time', units='MW/m^2', label='Max q (ELM alpha)')
+   endif
+;
+	rc=putdata(qmax(sti:eti), stepid='variable', $
+		group='/'+prefix, name='peakpower_pos', $
+		dimensions='time', units='m', label='R(peak q)')
+
+	rc=putdata(ptot(sti:eti), stepid='variable', $
+		group='/'+prefix, name='ptot', $
+		dimensions='time', units='MW', label='P(tot)')
+
+
+	rc=putdata(qprofiles, stepid='variable', $
+		group='/'+prefix, name='qprofile', $
+		dimensions='radius, time', units='MW/m^2', label='q_profile')
+
+	rc=putdata(tprofiles, stepid='variable', $
+		group='/'+prefix, name='tprofile', $
+		dimensions='radius, time', units='MW/m^2', label='t_profile')
+
+   if (nalpha gt 1) then begin
+		rc=putdata(qprofiles_elm, stepid='variable', $
+			group='/'+prefix, name='qprofile_elm', $
+			dimensions='radius, time', units='MW/m^2', label='q_profile_elm')
+   endif
+
+	rc=putdata(numsatpix, stepid='variable', $
+		group='/'+prefix, name='satpixels', $
+		units='', label='Num sat. pix.', dimensions='value')
+
+;write out the radius
+     
+	rc=putdata(rout, stepid='variable', $
+		group='/'+prefix, name='rcoord', $
+		units='m', label='R coord', dimensions='radius')
+
+; energies
+;FL 24/11/2003: adding PTOT and ETOT trace
+	rc=putdata(ptot(sti:eti), stepid='variable', $
+		group='/'+prefix, name='ptot', $
+		dimensions='time', units='MW', label='P(tot)')
+
+	rc=putdata(1.e3*etot(sti:eti), stepid='variable', $
+		group='/'+prefix, name='etot', $
+		dimensions='time', units='kJ', label='E(tot)')
+
+	rc=putdata(1.e3*etotsum(sti:eti), stepid='variable', $
+		group='/'+prefix, name='etotsum', $
+		dimensions='time', units='kJ', label='E_sum(tot)')
+
+    if (nalpha gt 1) then begin
+
+	rc=putdata(ptot_elm(sti:eti), stepid='variable', $
+		group='/'+prefix, name='ptot_elm', $
+		dimensions='time', units='MW', label='P(tot) ELM')
+
+	rc=putdata(1.e3*etot_elm(sti:eti), stepid='variable', $
+		group='/'+prefix, name='etot_elm', $
+		dimensions='time', units='kJ', label='E(tot) ELM')
+
+	rc=putdata(1.e3*etotsum_elm(sti:eti), stepid='variable', $
+		group='/'+prefix, name='etotsum_elm', $
+		dimensions='time', units='kJ', label='E_sum(tot) ELM')
+
+   endif
+    
+; Some Info about the TEMPERATURE
+; time evolution of maxima/minima
+   maxtemp=fltarr(max_times)
+   for i=0l,max_times-1 do begin
+      maxtemp(i)=max(tprofiles(*,i))
+   endfor
+
+	rc=putdata(maxtemp(sti:eti), stepid='variable', $
+		group='/'+prefix, name='temperature', $
+		dimensions='time', units='Degrees C', label='Max temp.')
+
+;  write out the configuration
+	rc=putdata(s, stepid='variable', $
+		group='/'+prefix, name='camera_view', dimensions='value')
+
+;  write out the value of alpha used
+	rc=putdata([alphaconst[0]], stepid='variable', $
+			group='/'+prefix, name='alphaconst', $
+			units='kW.m^-2', label='Alpha', dimensions='value')
+
+  if (nalpha gt 1) then begin
+		rc=putdata([alphaconst[1]], stepid='variable', $
+			group='/'+prefix, name='alphaconst_elm', $
+			units='kW.m^-2', label='Alpha ELM', dimensions='value')
+   endif
+
+;  write out the rstart         
+	rc=putdata([r_start], stepid='variable', $
+		group='/'+prefix, name='r_start', $
+		units='m', label='r_start', dimensions='value')
+
+;  write out the phistart         
+	rc=putdata([phi_start], stepid='variable', $
+		group='/'+prefix, name='phi_start', $
+		units='degrees', label='phi_start', dimensions='value')
+
+;  write out the zstart         
+	rc=putdata([z_start], stepid='variable', $
+		group='/'+prefix, name='z_start', $
+		units='m', label='z_start', dimensions='value')
+
+;  write out the rextent        
+	rc=putdata([r_extent], stepid='variable', $
+		group='/'+prefix, name='r_extent', $
+		units='m', label='r_extent', dimensions='value')
+
+	rc=putdata([phi_extent], stepid='variable', $
+		group='/'+prefix, name='phi_extent', $
+		units='degrees', label='phi_extent', dimensions='value')
+
+	rc=putdata([z_extent], stepid='variable', $
+		group='/'+prefix, name='z_extent', $
+		units='degrees', label='z_extent', dimensions='value')
+
     """
     pass
     raise NotImplementedError
