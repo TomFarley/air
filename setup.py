4ae39a307eb5058473786e9ebb0a2dbf72646556
Farley, Tom <tom.farley@ukaea.uk>
2020-04-02 09:37:13 +0100

    Add install requries dependencies of matplotlib and opencv-python

--- setup.py
+++ setup.py
@@ -1,23 +1,33 @@
 from setuptools import setup, find_packages
 import os
 
-setup(name='fire',
-      version='2.0',
-      description='Fusion Infra-Red Experiments analysis tool',
-      url='git@git.ccfe.ac.uk/MAST-U_Scheduler/air',
-      author='tfarley',
-      author_email='tom.farley@ukaea.uk',
-      # license=ccfepyutils.__license__,
-      packages=['fire'],
-      # packages=find_packages(exclude=['docs', 'external', 'misc', 'tests', 'third_party']),
-      package_data={
-            'fire': ['settings/*'],
-      },
-      include_package_data=True,
-      install_requires=["numpy >= 1.12.0", "scipy", "xarray", "pandas", "future-fstrings"],
-      python_requires='>=3',
-      setup_requires=['pytest-runner'],
-      test_suite='tests.test_suite_slow',
-      tests_require=['pytest-cov'],
-      zip_safe=False,
-      long_description=open('README.md').read())
+setup(
+    name='fire',
+    version='2.0',
+    description='Fusion Infra-Red Experiments analysis tool',
+    url='git@git.ccfe.ac.uk/MAST-U_Scheduler/air',
+    author='tfarley',
+    author_email='tom.farley@ukaea.uk',
+    # license=ccfepyutils.__license__,
+    packages=['fire'],
+    # packages=find_packages(exclude=['docs', 'external', 'misc', 'tests', 'third_party']),
+    package_data={
+        'fire': ['settings/*'],
+    },
+    include_package_data=True,
+    install_requires=[
+        "numpy >= 1.12.0",
+        "scipy",
+        "xarray",
+        "pandas",
+        "matplotlib",
+        "opencv-python",
+        "future-fstrings",  # tmp
+        ],
+    python_requires='>=3',
+    setup_requires=['pytest-runner'],
+    test_suite='tests.test_suite_slow',
+    tests_require=['pytest-cov'],
+    zip_safe=False,
+long_description=open('README.md').read()
+)
