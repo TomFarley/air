5ab6d2bc76810c67b856ea5ebb44ae0dc0e08b10
Farley, Tom <tom.farley@ukaea.uk>
2020-04-21 09:39:25 +0100

    Refine plot layouts

--- fire/plotting/debug_plots.py
+++ fire/plotting/debug_plots.py
@@ -12,7 +12,7 @@ import logging
 import matplotlib.pyplot as plt
 
 from fire.plotting.image_figures import (figure_imshow, figure_frame_data, plot_outlier_pixels, figure_analysis_path,
-                                         figure_spatial_res_max, figure_spatial_res_x, figure_spatial_res_y, plot_spatial_res_hist)
+                                         figure_spatial_res_max, figure_spatial_res_x, figure_spatial_res_y, plot_image_data_hist)
 from fire.plotting.path_figures import figure_path
 from fire.plotting.plot_tools import annotate_axis
 from fire.camera.image_processing import find_outlier_pixels
@@ -40,28 +40,27 @@ def debug_movie_data(data, aspect='equal'):
     figure_frame_data(data, key='nuc_frame', ax=ax, n=None, label_outliers=True, aspect=aspect, show=False)
 
     ax = axes[4]
-    figure_imshow(data, 'nuc_frame', ax=ax, show=False)
+    plot_image_data_hist(data, key='nuc_frame', xlabel='NUC frame DL', ax=ax, show=False)
 
     # TODO: Plot peak frame intensity variation
-    # TODO: plot NUC frame
 
     plt.tight_layout()
     plt.show()
 
-def debug_spatial_coords(data, aspect='equal'):
+def debug_spatial_coords(data, aspect='equal', axes_off=True):
     fig, axes = plt.subplots(3, 3, num='spatial coords', figsize=(13, 13))
     axes = axes.flatten()
 
     # Frame data
     ax = axes[0]
-    figure_frame_data(data, ax=ax, label_outliers=True, aspect=aspect, show=False)
+    figure_frame_data(data, ax=ax, label_outliers=True, aspect=aspect, axes_off=False, show=False)
 
     # Spatial coords
     axs = axes[1:]
-    keys = ['x_im', 'y_im', 'z_im', 'R_im', 'phi_deg_im', 's_global_im', 'sector_im', 'wire_frame', 'ray_lengths']
+    keys = ['x_im', 'y_im', 'R_im', 'phi_deg_im', 'z_im', 's_global_im', 'sector_im', 'ray_lengths', 'wire_frame']
     for ax, key in zip(axs, keys):
         try:
-            figure_imshow(data, key=key, ax=ax, show=False)
+            figure_imshow(data, key=key, ax=ax, axes_off=axes_off, show=False)
         except KeyError as e:
             logger.warning(f'Could not plot {str(e)}')
 
@@ -86,7 +85,8 @@ def debug_spatial_res(data, aspect='equal'):
 
     # Spatial res hist
     ax = axes[2]
-    plot_spatial_res_hist(data, ax=ax, show=False, save_fn=None)
+    plot_image_data_hist(data, key='spatial_res_max', ax=ax, xlabel='spatial res (max) [m]', log_x=True,
+                         show=False, save_fn=None)
 
     # Spatial res
     ax = axes[3]
@@ -99,19 +99,41 @@ def debug_spatial_res(data, aspect='equal'):
     plt.tight_layout()
     plt.show()
 
-def debug_analysis_path(image_data, path_data=None):
+def debug_surfaces(image_data, aspect='equal'):
+    ig, axes = plt.subplots(1, 3, num='surfaces', figsize=(13, 6))
+    axes = axes.flatten()
+
+    # Frame data
+    ax = axes[0]
+    figure_frame_data(image_data, ax=ax, aspect=aspect, show=False)
+
+    # Surface id
+    ax = axes[1]
+    key = 'surface_id'
+    figure_imshow(image_data, key=key, ax=ax, axes_off=True, show=False)
+
+    ax = axes[2]
+    key = 'material_id'
+    figure_imshow(image_data, key=key, ax=ax, axes_off=True, show=False)
+
+    plt.tight_layout()
+    plt.show()
+
+def debug_analysis_path(image_data, path_data=None, path_name='path0'):
+    path = path_name
     fig = plt.figure(constrained_layout=True, num='analysis paths')
     gs = fig.add_gridspec(ncols=2, nrows=5, width_ratios=[1, 3])
 
     ax1 = fig.add_subplot(gs[:4, 0])
 
-    figure_analysis_path(path_data, image_data, key='frame_data', ax=ax1, frame_border=True,
+    figure_analysis_path(path_data, image_data, key='frame_data', path_name=path_name, ax=ax1, frame_border=True,
                          show=False, image_kwargs=dict(add_colorbar=False, axes_off=True))
 
     # Line plots of parameters along path
     if path_data:
         share_x = None
-        keys = ('frame_data_path', 's_global_im_path', 'spatial_res_max_path', 'sector_im_path', 'material_id_path')
+        keys = (f'frame_data_{path}', f's_global_{path}', f'spatial_res_max_{path}', f'sector_{path}',
+                f'surface_id_{path}')
         for i_row, key in enumerate(keys):
             ax = fig.add_subplot(gs[i_row, 1], sharex=share_x)
 
@@ -140,7 +162,7 @@ def debug_analysis_path(image_data, path_data=None):
     plt.show()
 
 def debug_temperature(data):
-    figure_imshow(data, key='frame_temperature', show=True)
+    figure_imshow(data, key='temperature_im', show=True)
 
 
 if __name__ == '__main__':
