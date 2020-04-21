d8e96038ebf2a3041e2700eceb79735d9de4c295
Farley, Tom <tom.farley@ukaea.uk>
2020-04-21 12:26:03 +0100

    Only add MASTU tests if uda available

--- tests/test_suite_fast.py
+++ tests/test_suite_fast.py
@@ -11,9 +11,9 @@ from pathlib import Path
 # print(sys.path)
 # print(Path('.').resolve())
 # print(Path(__file__).resolve())
-from tests.unit import test_io_uda
+
 from tests.unit import test_io_ipx
-from tests.unit import test_interfaces, test_calcam, test_utils, test_camera_shake
+from tests.unit import test_interfaces, test_calcam, test_utils, test_camera_shake, test_s_coordinate
 
 try:
     import pyuda
@@ -28,13 +28,18 @@ suite  = unittest.TestSuite()
 
 # add tests to the test suite
 if pyuda:
+    from tests.unit import test_mast_u
+    from tests.unit import test_io_uda
     suite.addTests(loader.loadTestsFromTestCase(test_io_uda.TestIoUdaFast))
-suite.addTests(loader.loadTestsFromTestCase(test_io_ipx.TestIoIpxFast))
+    suite.addTests(loader.loadTestsFromTestCase(test_mast_u.TestMastU))
+
 # suite.addTests(loader.loadTestsFromModule(test_io_uda))
-suite.addTests(loader.loadTestsFromModule(test_interfaces))
 suite.addTests(loader.loadTestsFromModule(test_calcam))
-suite.addTests(loader.loadTestsFromModule(test_utils))
 suite.addTests(loader.loadTestsFromModule(test_camera_shake))
+suite.addTests(loader.loadTestsFromModule(test_interfaces))
+suite.addTests(loader.loadTestsFromTestCase(test_io_ipx.TestIoIpxFast))
+suite.addTests(loader.loadTestsFromModule(test_s_coordinate))
+suite.addTests(loader.loadTestsFromModule(test_utils))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
--- tests/test_suite_slow.py
+++ tests/test_suite_slow.py
@@ -6,18 +6,36 @@ For inmports to resolve, run test suite from top level air directory (not from t
 
 import unittest
 
-from tests.unit import test_io_uda, test_io_ipx
-from tests.unit import test_interfaces, test_calcam, test_utils
+from tests.unit import test_io_ipx
+from tests.unit import test_interfaces, test_calcam, test_utils, test_camera_shake, test_s_coordinate
+
+try:
+    import pyuda
+    client = pyuda.Client()
+except ImportError as e:
+    print(f'Failed to import pyuda. ')
+    pyuda = False
 
 # initialize the test suite
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 # add tests to the test suite
-suite.addTests(loader.loadTestsFromModule(test_io_ipx))
-suite.addTests(loader.loadTestsFromModule(test_io_uda))
-suite.addTests(loader.loadTestsFromModule(test_interfaces))
+if pyuda:
+    from tests.unit import test_mast_u
+    from tests.unit import test_io_uda
+    suite.addTests(loader.loadTestsFromTestCase(test_io_uda.TestIoUdaSlow))
+    suite.addTests(loader.loadTestsFromTestCase(test_mast_u.TestMastU))
+
+
+# add tests to the test suite
+suite.addTests(loader.loadTestsFromTestCase(test_io_ipx.TestIoIpxSlow))
+
 suite.addTests(loader.loadTestsFromModule(test_calcam))
+suite.addTests(loader.loadTestsFromModule(test_camera_shake))
+suite.addTests(loader.loadTestsFromModule(test_interfaces))
+suite.addTests(loader.loadTestsFromTestCase(test_io_ipx.TestIoIpxFast))
+suite.addTests(loader.loadTestsFromModule(test_s_coordinate))
 suite.addTests(loader.loadTestsFromModule(test_utils))
 
 # initialize a runner, pass it your suite and run it
