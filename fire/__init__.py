240e49089968bbb78b3fcabdffb7d5a2620a5610
Farley, Tom <tom.farley@ukaea.uk>
2020-08-10 16:51:05 +0100

    Set up logging for FIRE module using yaml config file

--- fire/__init__.py
+++ fire/__init__.py
@@ -1,26 +1,41 @@
 # -*- coding: future_fstrings -*-
 import logging
+import logging.config
+import yaml
 from pathlib import Path
 
 import pandas as pd
 import xarray as xr
-
 # Set up logger for module
-logging.basicConfig()
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logging.basicConfig(
+    # format="%(levelname)s:%(module)s:%(funcName)s:%(lineno)s: %(message)s",
+    )
+
+with open('logging_config.yaml', 'r') as f:
+    config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+
+logger_fire = logging.getLogger(__name__)
+logger_fire.setLevel(logging.DEBUG)
 
 # Store important paths for quick reference
 fire_paths = {'root': Path(__file__).parent}
 fire_paths['input_files'] = fire_paths['root'] / 'input_files'
 fire_paths['config'] = Path('~/.fire_config.json').expanduser()
 
-if not fire_paths['config'].exists():
+active_machine_plugin = None
+active_calcam_calib = None
+
+def copy_default_user_settings(replace_existing=False):
     # Set up fire config file
     # TODO: skip if on scheduler?
     fire_config_template_path_fn = fire_paths['input_files'] / 'user' / 'fire_config.json'
-    fire_paths['config'].write_text(fire_config_template_path_fn.read_text())
-    print(f'Coppied template fire config to "{fire_paths["config"]}" from "{fire_config_template_path_fn}"')
+    if (not fire_paths['config'].exists()) or replace_existing:
+        fire_paths['config'].write_text(fire_config_template_path_fn.read_text())
+        logger_fire.info(f'Copied template fire config file to "{fire_paths["config"]}" from '
+                    f'"{fire_config_template_path_fn}"')
+
+copy_default_user_settings(replace_existing=False)
 
 # Update pandas display settings
 # Double max width of displayed output in terminal so doesn't wrap over many lines
@@ -34,6 +49,7 @@ xr.set_options(**{'cmap_sequential': 'gray'})
 
 # Import FIRE sub-modules
 from .interfaces import interfaces
+from.misc import utils
 # from .theodor import theo_mul_33 as theodor
 
 # from .. import tests
