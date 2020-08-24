843d8ab6e6e3e36837b3f2d71d4b1611e79f4cf6
Farley, Tom <tom.farley@ukaea.uk>
2020-08-24 15:49:44 +0100

    Add function write_to_output_format

---
+++ fire/plugins/plugins_output_format.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""
+
+
+Created: 
+"""
+
+import logging
+from typing import Union, Sequence, Optional, Dict, Callable
+
+import numpy as np
+
+from fire.geometry.s_coordinate import get_s_coord_global_r, get_s_coord_path_ds
+from fire.misc.utils import filter_kwargs
+
+logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+
+coord = Union[float, np.ndarray]
+
+def write_to_output_format(file_format_plugins: Dict[str, Callable], path_data, image_data, path_names,
+                           variable_names_path, variable_names_time, variable_names_image,
+                           device_info, meta_data, header_info=None,
+                           fn_output=None, path_output=None):
+
+    outputs = {}
+    write_plugin_attr = 'write_output'
+    for plugin, attrs in file_format_plugins.items():
+        if write_plugin_attr in attrs:
+            # try:
+            func = attrs[write_plugin_attr]
+            # kwargs order of precedence: kwargs!=None -> Plugin module defaults -> Plugin function defaults
+            kwargs = filter_kwargs(attrs, func, remove_from_input=False)
+            kws = {k: v for k, v in dict(fn_output=fn_output, path_output=path_output).items() if v is not None}
+            kwargs.update(kws)
+            outputs[plugin] = func(path_data, image_data, path_names,
+                                   variable_names_path, variable_names_time, variable_names_image,
+                                   header_info, device_info, meta_data, **kwargs)
+            # except KeyError as e:
+            #     pass
+    return outputs
+
+if __name__ == '__main__':
+    pass
