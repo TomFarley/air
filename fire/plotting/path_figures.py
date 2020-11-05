3957a284101b392adcd5e8b867349aa75ff5cb5e
Farley, Tom <tom.farley@ukaea.uk>
2020-11-05 11:17:46 +0000

    Add figure_path_2d

--- fire/plotting/path_figures.py
+++ fire/plotting/path_figures.py
@@ -15,12 +15,27 @@ import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as plt
 
+from fire.plotting.plot_tools import get_fig_ax
+
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.DEBUG)
 
 
-def figure_path(data, key, slice=None, ax=None, plot_kwargs=None):
-    """Plot profile of variable given by key along analysis path"""
+def figure_path_1d(data, key, slice=None, ax=None, plot_kwargs=None):
+    """Plot profile of variable given by key along analysis path
+
+    Args:
+        data: Dataset containing plot data
+        key: Name of variable in dataset to plot
+        slice: (Temporal) slice to plot
+        ax: Matplotlib axis to plot to
+        plot_kwargs: Additional formatting args to pass to plt.plot
+
+    Returns: ax
+
+    """
+    fig, ax, ax_passed = get_fig_ax(ax)
+
     if slice is None:
         slice = {'n': np.floor(np.median(data['n']))}
     kws = {'color': None}
@@ -34,6 +49,27 @@ def figure_path(data, key, slice=None, ax=None, plot_kwargs=None):
     data_plot.plot.line(ax=ax, **kws)
     ax.title.set_fontsize(10)
 
+    return ax
+
+def figure_path_2d(data, key, slice=None, ax=None, plot_kwargs=None):
+    fig, ax, ax_passed = get_fig_ax(ax)
+
+    if slice is None:
+        raise NotImplementedError('Subset of frames/time range')
+        # slice = {'n': np.floor(np.median(data['n']))}
+
+    kws = {'cmap': None}
+    if isinstance(plot_kwargs, dict):
+        kws.update(plot_kwargs)
+
+    data_plot = data[key]
+    if slice is not None:
+        data_plot = data_plot.sel(slice)
+
+    data_plot.plot(ax=ax, robust=True, **kws)
+    ax.title.set_fontsize(10)
+
+    return ax
 
 if __name__ == '__main__':
     pass
