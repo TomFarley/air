f0e0f1a70fd24adf91b8d5d586e1435c752c6660
Farley, Tom <tom.farley@ukaea.uk>
2021-03-09 15:58:14 +0000

    Add plotting meta data for more variables

--- fire/misc/data_structures.py
+++ fire/misc/data_structures.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import numpy as np
 import xarray as xr
 
+from fire.misc.utils import make_iterable
+
 logger = logging.getLogger(__name__)
 
 # TODO: Move meta data defaults to json file
@@ -46,12 +48,20 @@ meta_defaults_default = {
                   'description': 'Machine toroidal coordinate in degrees (phi=0 along x axis)'},
     's_global': {'label': 'Divertor tile S coordinate', 'units': 'm', 'symbol': '$S$',
                   'description': 'Spatial coordinate along surface of PFCs'},
+    'i_path': {'label': 'Array index along analysis path', 'units': '', 'symbol': '$i_{path}$',
+                  'description': 'Array index along analysis path (alternative to R/s_global)'},
     'surface_id': {'label': 'Surface id', 'units': 'count', 'symbol': '$ID_{surface}$',
                   'description': 'Integer ID specifying what machine component/tile etc is visible at that pixel'},
+    'temperature_peak': {'label': 'Peak temperature', 'units': '$^\circ$C', 'symbol': '$T_{peak}$',
+                  'description': 'Peak target temperature along analysis path as a function of time'},
     'heat_flux_peak': {'label': 'Peak heat flux', 'units': 'MWm$^{-2}$', 'symbol': '$q_{\perp,peak}$',
-                  'description': 'Peak target heat flux as a function of time'},
+                  'description': 'Peak target heat flux along analysis path as a function of time'},
     'heat_flux_r_peak': {'label': 'Target location', 'units': 'm', 'symbol': '$R_{target}$',
                   'description': 'Radial location of peak target heatflux'},
+    'temperature_r_peak': {'label': 'Peak temperature location', 'units': 'm', 'symbol': '$R_{T,max}$',
+                      'description': 'Radial location of peak target temperature'},
+    'xpx/clock/lwir-1': {'label': 'LWIR 1 trigger signal', 'units': 'V', 'symbol': '$V_{LWIR1, trig}$',  # TODO: move to uda_utils
+                         'description': 'Voltage of IRCAM1 LWIR HL04 datac loop trigger signal (xpx/clock/lwir-1)'},
 
 }
 # TODO: Add dict of alternative names for each variable in meta_defaults_default eg 'S', 's_global'
@@ -65,7 +75,7 @@ param_aliases = {
 for key in meta_defaults_default:
     meta_defaults_default[key]['long_name'] = meta_defaults_default[key]['symbol']  # Set longname for xarray plots
 
-def init_data_structures() -> Tuple[dict, dict, xr.Dataset, dict]:
+def init_data_structures() -> Tuple[dict, dict, xr.Dataset, xr.Dataset, dict, dict]:
     """Return empty data structures for population by and use in fire code
 
     :return: settings, files, data, meta_data
@@ -74,10 +84,12 @@ def init_data_structures() -> Tuple[dict, dict, xr.Dataset, dict]:
 
     settings = {}
     files = {}
-    data = xr.Dataset()
+    data_image = xr.Dataset()
+    data_path = xr.Dataset()
     meta_data = {}
+    meta_runtime = {}  # Used to collect together objects eg calcam.Calibration that may be hard to serialise
 
-    return settings, files, data, meta_data
+    return settings, files, data_image, data_path, meta_data, meta_runtime
 
 
 def movie_data_to_dataarray(frame_data, frame_times=None, frame_nos=None, meta_data=None, name='frame_data'):
@@ -177,10 +189,50 @@ def to_image_dataset(data, key='data'):
             'long_name': '$y_{pix}$',
             'units': '',
             'description': 'Camera y pixel coordinate'})
-        dataset = data_structures.attach_standard_meta_attrs(dataset, varname=key)
+        dataset = attach_standard_meta_attrs(dataset, varname=key)
         # TODO: Move to utils/data_structures?
         # TODO: fix latex display of axis labels
         # TODO: use this func in calcam_calibs get_surface_coords
     else:
         raise ValueError(f'Unexpected image data type {data}')
     return dataset
+
+
+def get_reduce_coord_axis_keep(data, coord_reduce):
+    # Get preserved dimension used for xr.apply_ufunc
+    ndim = data.values.ndim
+    if ndim == 2:
+        axis_reduce = list(data.dims).index(coord_reduce)
+        coord_keep = list(data.dims)
+        coord_keep.pop(data.dims.index(coord_reduce))
+        coord_keep = coord_keep[0]
+        axis_keep = list(data.dims).index(coord_keep)
+    elif ndim == 1:
+        coord_keep = None
+        axis_keep = None
+        axis_reduce = None
+    else:
+        raise ValueError(f'Too many dimensions: {data}')
+
+    return coord_keep, axis_keep, axis_reduce
+
+
+def reduce_2d_data_array(data_array, func_reduce, coord_reduce, func_args=()):
+    """Apply a numpy ufunc to reduce 2D DataArray dimensionality to 1D for plotting etc
+
+    Args:
+        data_array: Input 2D dataarray
+        func_reduce: np ufunc, eg np.mean
+        coord_reduce: Coordinate name to apply ufunc across (ie average across time, 't')
+        func_args: Any additional args to pass to ufunc eg percentile to pass to np.percentile
+
+    Returns: DataArray with coord_reduce compressed by func_reduce
+
+    """
+    func_args = make_iterable(func_args)
+    coord_keep, axis_keep, axis_reduce = get_reduce_coord_axis_keep(data_array, coord_reduce)
+    input_core_dims = ((coord_reduce,), ) + tuple(() for i in np.arange(len(func_args)))
+
+    data_array_reduced = xr.apply_ufunc(func_reduce, data_array, *func_args,
+                                                input_core_dims=input_core_dims, kwargs=dict(axis=axis_keep))
+    return data_array_reduced
