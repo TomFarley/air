9ac3539db8c0ed53fd3701893beb203acfd4c427
Farley, Tom <tom.farley@ukaea.uk>
2021-03-17 15:25:22 +0000

    Add logger import

--- fire/interfaces/__init__.py
+++ fire/interfaces/__init__.py
@@ -1,3 +1,5 @@
 """This submodule contains functions for interacting with standard FIRE tools such as calcam that are not swap-outable
 components like the plugin functions in fire/plugins.
 """
+import logging
+logger = logging.getLogger(__name__)
