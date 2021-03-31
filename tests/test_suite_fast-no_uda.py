512418c2103d85526818d92572818b64d1b04175
Farley, Tom <tom.farley@ukaea.uk>
2021-03-31 16:51:16 +0100

    Add UDA free test suits

---
+++ tests/test_suite_fast-no_uda.py
@@ -0,0 +1,46 @@
+"""
+
+For inmports to resolve, run test suite from top level air directory (not from tests directory) ie:
+~/repos/air $ ipython tests/test_suite_fast.py   OR
+~/repos/air $ python setup.py test
+"""
+
+import unittest, sys
+from pathlib import Path
+# print('file:', __file__, 'name:', __name__, 'package:', __package__)
+# print(sys.path)
+# print(Path('.').resolve())
+# print(Path(__file__).resolve())
+
+from tests.unit import test_io_ipx
+from tests.unit import test_interfaces, test_calcam, test_utils, test_camera_shake, test_s_coordinate
+
+pyuda = False
+# try:
+#     import pyuda
+#     client = pyuda.Client()
+# except ImportError as e:
+#     print(f'Failed to import pyuda. ')
+
+# initialize the test suite
+loader = unittest.TestLoader()
+suite  = unittest.TestSuite()
+
+# add tests to the test suite
+if pyuda:
+    from tests.unit import test_mast_u
+    from tests.unit import test_io_uda
+    suite.addTests(loader.loadTestsFromTestCase(test_io_uda.TestIoUdaFast))
+    suite.addTests(loader.loadTestsFromTestCase(test_mast_u.TestMastU))
+
+# suite.addTests(loader.loadTestsFromModule(test_io_uda))
+suite.addTests(loader.loadTestsFromModule(test_calcam))
+suite.addTests(loader.loadTestsFromModule(test_camera_shake))
+suite.addTests(loader.loadTestsFromModule(test_interfaces))
+suite.addTests(loader.loadTestsFromTestCase(test_io_ipx.TestIoIpxFast))
+suite.addTests(loader.loadTestsFromModule(test_s_coordinate))
+suite.addTests(loader.loadTestsFromModule(test_utils))
+
+# initialize a runner, pass it your suite and run it
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(suite)
