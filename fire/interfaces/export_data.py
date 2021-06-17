defeb317215f1995f1055297dcf6557e9d6258b3
Farley, Tom <tom.farley@ukaea.uk>
2021-06-17 10:47:56 +0100

    Add new skeleton files

---
+++ fire/interfaces/export_data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+
+
+Created: 
+"""
+
+import logging
+from typing import Union, Iterable, Sequence, Tuple, Optional, Any, Dict
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+logger.propagate = False
+
+from fire.interfaces.interfaces import write_csv
+
+if __name__ == '__main__':
+    pass
