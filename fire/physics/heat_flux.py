19700e7b9502936d0adb909c309ae8f425d573d7
Farley, Tom <tom.farley@ukaea.uk>
2021-09-08 15:52:24 +0100

    Add alpha param scan func

--- fire/physics/heat_flux.py
+++ fire/physics/heat_flux.py
@@ -10,6 +10,7 @@ Created:
 import logging
 from typing import Union, Iterable, Tuple, Optional, Sequence
 from pathlib import Path
+from collections import defaultdict
 
 import numpy as np
 import pandas as pd
@@ -20,13 +21,104 @@ import matplotlib.pyplot as plt
 from fire.interfaces.io_basic import read_csv
 from fire.misc.utils import safe_len
 from fire.misc import utils
+from fire import fire_paths
 
 logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 
+def theo_kwargs_for_path(material_ids, visible_materials, material_properties,
+                  force_material_sub_index=None, raise_on_multiple_materials=True):
+    if (force_material_sub_index is not None):
+        material_id = force_material_sub_index
+    else:
+        if (len(visible_materials) > 1) and raise_on_multiple_materials:
+            raise ValueError('Multiple materials for analysis path - beak up path')
+        else:
+            material_id = list(visible_materials.keys())[0]
+    material_name = visible_materials[material_id]
+    theo_kwargs = material_properties[material_name]
+    return material_name, theo_kwargs
 
-def calc_heatflux(t, temperatures, path_data, path_name, material_properties, visible_materials,
-                  force_material_sub_index=None):
+def scan_alpha_param(temperature_path, t, s_path, theo_kwargs, alpha_values, test=True, meta=(), verbose=True):
+    from fire import theodor
+    from fire.plotting import plot_tools
+    print(f'Performing alpha scan with values: {alpha_values}')
+    pulse, camera, machine = dict(meta).get('pulse'), dict(meta).get('camera'), dict(meta).get('machine')
+
+    fig, axes, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_heatmaps, {machine} {camera} {pulse}',
+                                                 ax_grid_dims=(2, int(np.ceil(len(alpha_values)/2))),
+                                                 figsize=(12, 12))
+    axes = axes.flatten()
+    data = {}
+    stats = defaultdict(list)
+    radial_average = defaultdict(list)
+    radial_average['temperature_av'].append(np.mean(temperature_path, axis=0))
+    radial_average['temperature_min'].append(np.min(temperature_path, axis=0))
+    radial_average['temperature_max'].append(np.max(temperature_path, axis=0))
+
+    for i, alpha in enumerate(alpha_values):
+        theo_kwargs['alpha_top_org'] = alpha
+        heat_flux, extra_results = theodor.theo_mul_33(temperature_path, t, s_path, test=test, verbose=verbose,
+                                                       **theo_kwargs)
+        heat_flux *= 1e-6
+        data[tuple(alpha)] = heat_flux.T
+        stats['max'].append(np.max(heat_flux))
+        stats['99%'].append(np.percentile(heat_flux, 99))
+        stats['98%'].append(np.percentile(heat_flux, 98))
+        stats['mean'].append(np.mean(heat_flux))
+        stats['median'].append(np.percentile(heat_flux, 50))
+        stats['2%'].append(np.percentile(heat_flux, 2))
+        stats['1%'].append(np.percentile(heat_flux, 1))
+        stats['min'].append(np.min(heat_flux))
+        radial_average['heat_flux'].append(np.mean(heat_flux, axis=0))
+        ax = axes[i]
+        im = ax.pcolor(heat_flux, cmap='coolwarm')
+        plot_tools.annotate_axis(ax, rf'$\alpha=${alpha}', loc='top left')
+        plt.colorbar(im, ax=ax)
+        plt.tight_layout()
+    plot_tools.annotate_providence(ax, meta_data=meta)
+    plot_tools.save_fig(fire_paths['figures'] / f'alpha_scan/heatmaps/'
+                                                f'alpha_scan_heatmaps-{machine}-{camera}-{pulse}.png', mkdir_depth=2)
+    plot_tools.show_if(True)
+
+    fig, ax, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_q_stats, {machine} {camera} {pulse}',
+                                               ax_grid_dims=(1, 1))
+    alphas_flat = [a[0] for a in alpha_values]
+    for stat, values in stats.items():
+        ax.plot(alphas_flat, values, marker='x', color=None, label=stat)
+    # ax.plot(alphas_flat, medians, marker='.', color=None, label='median')
+    # ax.plot(alphas_flat, maxs, marker='o', color=None, label='max')
+    ax.axhline(y=0, ls=':', color='k')
+    plot_tools.annotate_providence(ax, meta_data=meta)
+    plot_tools.legend(ax)
+    ax.set_xlabel(r'$\alpha$ [W/(m$^2$•K)]')
+    ax.set_ylabel(r'$q_\perp$ [MW]')
+    plot_tools.save_fig(fire_paths['figures'] / f'alpha_scan/q_stats/alpha_scan_q_stats-{machine}-{camera}-{pulse}.png',
+                        mkdir_depth=2)
+    plot_tools.show_if(show=True, tight_layout=True)
+
+    fig, axes, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_radial_av, {machine} {camera} {pulse}',
+                                               ax_grid_dims=(1, 2), sharex=True)
+    ax = axes[0]
+    for alpha, profile in zip(alphas_flat, radial_average['heat_flux']):
+        ax.plot(t, profile, marker='.', color=None, label=r'$q_{av}$, $\alpha$='+fr'{alpha}', alpha=0.7)
+    ax.set_ylabel('$q_{\perp,av}$ [MW/m$^2$]')
+    plot_tools.annotate_providence(ax, meta_data=meta)
+    plot_tools.legend()
+
+    ax = axes[1]
+    ax.plot(t, radial_average['temperature_av'][0], marker='.', ls='-', color='k', label=r'$T_{av}')
+    ax.plot(t, radial_average['temperature_min'][0], marker='.', ls='-', color=None, label=r'$T_{min}')
+    ax.plot(t, radial_average['temperature_max'][0], marker='.', ls='-', color=None, label=r'$T_{max}')
+    plot_tools.legend()
+    ax.set_xlabel('$t$ [s]')
+    ax.set_ylabel('$T$ [$^\circ$C]')
+    plot_tools.save_fig(fire_paths['figures'] / f'alpha_scan/radial_av/'
+                                                f'alpha_scan_radial_av-{machine}-{camera}-{pulse}.png',mkdir_depth=2)
+    plot_tools.show_if(show=True, tight_layout=True)
+
+
+def calc_heatflux(t, temperatures, path_data, path_name, theo_kwargs, force_material_sub_index=None, meta=()):
     """
 
     Args:
@@ -43,6 +135,8 @@ def calc_heatflux(t, temperatures, path_data, path_name, material_properties, vi
 
     """
     from fire import theodor
+    meta = dict(meta)
+
     t = np.array(t)
     path = path_name
 
@@ -51,8 +145,8 @@ def calc_heatflux(t, temperatures, path_data, path_name, material_properties, vi
     dt_mode = stats.mode(dt).mode
     mask_const_dt = np.isclose(dt, dt_mode, atol=9e-7, rtol=5e-3)
     if not np.all(mask_const_dt):
-        raise ValueError(f'Time axis of data supplied to THEODOR is not uniform. mode={dt_mode}. dt other: '
-                         f'{dt[~mask_const_dt]}')
+        logger.warning(f'Time axis of data supplied to THEODOR is not uniform. mode={dt_mode}, '
+                       f'n_diff={np.sum(~mask_const_dt)}. dt other: {dt[~mask_const_dt]}')
     # TODO: Check analysis path for jumps/tile gaps etc
 
     # TODO generalise to other path names (i.e. other than 'path')
@@ -77,10 +171,6 @@ def calc_heatflux(t, temperatures, path_data, path_name, material_properties, vi
         s_path = np.array(path_data[f'R_{path}'])
     if np.any(np.isnan(s_path)):
         s_path = utils.interpolate_out_nans(s_path)
-
-    material_id = list(material_ids)[0] if (force_material_sub_index is None) else force_material_sub_index
-    material_name = visible_materials[material_id]
-    theo_kwargs = material_properties[material_name]
 
     # TODO: Understand when two element alpha_top_org values should be used
     if False:
@@ -124,24 +214,32 @@ def calc_heatflux(t, temperatures, path_data, path_name, material_properties, vi
 
     """
     Required arguments:
-    data            - float temperature array (location,time)
-    time            - temporal coordinates of data[1]
-    location        - spatial coordinates of data[0]
-    d_target        - target thickness in m
-    alpha_bot       - heat transmission coefficient at the bottom of the tile
+    data            - float temperature array (location,time) [C]
+    time            - temporal coordinates of data[1] [s]
+    location        - spatial coordinates of data[0] [m]
+    d_target        - target thickness [m]
+    alpha_bot       - heat transmission coefficient at the bottom of the tile [W/(m2•K)] 
     alpha_top_org   - heat transmission coefficient at the top of the tile. Not sure when this should have two elements 
-    diff            - heat diffusion coefficient at [0,500,1000 C] m^2/s
-    lam             - heat conduction coefficient (at [0,500,1000 C]) W/m/K
+    diff            - heat diffusion coefficient at [0,500,1000 C] [m^2/s]
+    lam             - heat conduction coefficient (at [0,500,1000 C]) [W/m/K]
     aniso           - anisotropy of vertical and tangential heat conduction. Ratio of 1.0 -> isotropic
     
     NOTES:
     - The remaining arguments are for visualisation/debugging and can be ignored
     - If location[x] is not the same size as the x dimension of data[x,t], the code uses the array indices as location[x].
     """
+    logger.info(f'Calling THEODOR with kwargs: {theo_kwargs}')
 
-    heat_flux, extra_results = theodor.theo_mul_33(temperature_path, t, s_path, test=True, verbose=True,
-                                                   **theo_kwargs)
+    heat_flux, extra_results = theodor.theo_mul_33(temperature_path, t, s_path, test=True, verbose=True, **theo_kwargs)
     #               d_target, alpha_bot, alpha_top, diff, lam, aniso, x_Tb=x_Tb, y_Tb=y_Tb,
+
+    scan_alphas = False
+    # scan_alphas = True
+    if scan_alphas:
+        # alpha_values = [[70000], [10000]]
+        alpha_values = [[a] for a in np.arange(10e2, 250e3, 25e3)]
+        alpha_values = [np.array(a) for a in alpha_values]
+        scan_alpha_param(temperature_path, t, s_path, theo_kwargs, alpha_values, test=True, meta=meta, verbose=True)
 
     # Convert W -> MW
     heat_flux *= 1e-6
