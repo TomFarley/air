c00c5e62d6f8abc74d2de46c731869700bbabf3a
Farley, Tom <tom.farley@ukaea.uk>
2021-11-30 11:00:17 +0000

    Add 'validation' directory for testing fire output

---
+++ fire/validation/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+
+
+Created: 
+"""
+
+import logging
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    pass
--- fire/validation/compare_to_past_ir_scheduler_results.py
+++ fire/validation/compare_to_past_ir_scheduler_results.py
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     # signals = ['AIR_TPROFILE_OSP']
     signals = ['AIR_QPROFILE_OSP']
     # pulse = 23586  # Full frame with clear spatial calibration
-    pulse = 26505  # Full frame OSP only louvre12d, 1D analysis profile, HIGH current - REQUIRES NEW CALCAM CALIBRATION
+    # pulse = 26505  # Full frame OSP only louvre12d, 1D analysis profile, HIGH current - REQUIRES NEW CALCAM CALIBRATION
+    pulse = 29541  # Full frame OSP only louvre12d, 1D analysis profile, HIGH current - REQUIRES NEW CALCAM CALIBRATION
     uda_utils.plot_uda_signals_individually(pulse, signals=signals, diagnostic_alias='air', min_rank=2, verbose=True)
     pass
