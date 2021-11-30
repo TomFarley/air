125730eafc37ef8d6e48ca4cfb282aa1515a8e83
Farley, Tom <tom.farley@ukaea.uk>
2021-11-30 09:57:18 +0000

    Add email example

--- fire/interfaces/email_notifications.py
+++ fire/interfaces/email_notifications.py
@@ -43,6 +43,26 @@ def email_warning():
     os.system(command + address)
     return 0
 
+def email():
+    """
+    https://realpython.com/python-send-email/
+    """
+    import smtplib, ssl
+
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "my@gmail.com"  # Enter your address
+    receiver_email = "your@gmail.com"  # Enter receiver address
+    password = input("Type your password and press enter: ")
+    message = """\
+    Subject: Hi there
+
+    This message is sent from Python."""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
 
 if __name__ == '__main__':
     pass
