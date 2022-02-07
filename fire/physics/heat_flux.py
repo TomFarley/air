3ffb33b7d36240cf014a3c7adb98b3dbd72e32a5
Farley, Tom <tom.farley@ukaea.uk>
2022-02-07 14:22:48 +0000

    Update alpha scan figures

--- fire/physics/heat_flux.py
+++ fire/physics/heat_flux.py
@@ -18,10 +18,11 @@ import xarray as xr
 from scipy import stats
 import matplotlib.pyplot as plt
 
+from fire.interfaces import io_basic, uda_utils
 from fire.interfaces.io_basic import read_csv
 from fire.misc.utils import safe_len
 from fire.misc import utils
-
+from fire import theodor
 
 logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
@@ -39,20 +40,16 @@ def theo_kwargs_for_path(material_ids, visible_materials, material_properties,
     theo_kwargs = material_properties[material_name]
     return material_name, theo_kwargs
 
-def scan_alpha_param(temperature_path, t, s_path, theo_kwargs, alpha_values, test=True, meta=(), verbose=True):
-    from fire import theodor, base_paths
-    from fire.plotting import plot_tools
-    print(f'Performing alpha scan with values: {alpha_values}')
-    pulse, camera, machine = dict(meta).get('pulse'), dict(meta).get('camera'), dict(meta).get('machine')
+def calc_alpha_scan(temperature_path, t, s_path, theo_kwargs, alpha_values, test=True, meta=(),
+                     r_path=None, path_fn_pickle=None, verbose=True):
+    print(f'Performing alpha scan calculation with values: {alpha_values}')
+    pulse, diag_tag_raw, machine = dict(meta).get('pulse'), dict(meta).get('diag_tag_raw'), dict(meta).get('machine')
+    diag_tag_analysed = uda_utils.get_analysed_diagnostic_tag(diag_tag_raw)
 
     alpha_passed = theo_kwargs.get('alpha_top_org')
 
-    fig, axes, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_heatmaps, {machine} {diag_tag_raw} {pulse}',
-                                                 ax_grid_dims=(2, int(np.ceil(len(alpha_values)/2))),
-                                                 figsize=(12, 12))
-    axes = axes.flatten()
     data = {}
-    stats = defaultdict(list)
+    stats_dict = defaultdict(list)
     radial_average = defaultdict(list)
     radial_average['temperature_av'].append(np.mean(temperature_path, axis=0))
     radial_average['temperature_min'].append(np.min(temperature_path, axis=0))
@@ -66,30 +63,94 @@ def scan_alpha_param(temperature_path, t, s_path, theo_kwargs, alpha_values, tes
                                                        **theo_kwargs)
         heat_flux *= 1e-6
         data[tuple(alpha)] = heat_flux.T
-        stats['max'].append(np.max(heat_flux))
-        stats['99%'].append(np.percentile(heat_flux, 99))
-        stats['98%'].append(np.percentile(heat_flux, 98))
-        stats['mean'].append(np.mean(heat_flux))
-        stats['median'].append(np.percentile(heat_flux, 50))
-        stats['2%'].append(np.percentile(heat_flux, 2))
-        stats['1%'].append(np.percentile(heat_flux, 1))
-        stats['min'].append(np.min(heat_flux))
+        stats_dict['max'].append(np.max(heat_flux))
+        stats_dict['99%'].append(np.percentile(heat_flux, 99))
+        stats_dict['98%'].append(np.percentile(heat_flux, 98))
+        stats_dict['mean'].append(np.mean(heat_flux))
+        stats_dict['median'].append(np.percentile(heat_flux, 50))
+        stats_dict['2%'].append(np.percentile(heat_flux, 2))
+        stats_dict['1%'].append(np.percentile(heat_flux, 1))
+        stats_dict['min'].append(np.min(heat_flux))
         radial_average['heat_flux'].append(np.mean(heat_flux, axis=0))
+
+    # Pickle data
+    if path_fn_pickle is not None:
+        from fire.interfaces.io_basic import pickle_dump
+        pickle_dump(dict(alpha_values=alpha_values, data=data, stats=stats_dict, radial_average=radial_average, t=t,
+                    s_path=s_path, theo_kwargs=theo_kwargs, pulse=pulse, diag_tag_raw=diag_tag_raw, machine=machine),
+                    path=path_fn_pickle, verbose=True)
+
+    return data, stats_dict, radial_average
+
+def plot_alpha_scan(data, stats_dict, radial_average, alpha_values, t, s_path, r_path, meta, alpha_passed=None):
+    from fire import theodor, base_paths
+    from fire.plotting import plot_tools, debug_plots
+    from fire.interfaces import uda_utils
+
+    pulse, diag_tag_raw, machine = dict(meta).get('pulse'), dict(meta).get('diag_tag_raw'), dict(meta).get('machine')
+    diag_tag_analysed = uda_utils.get_analysed_diagnostic_tag(diag_tag_raw)
+
+    # i_slice_s = 81  # R=0.829, s=2.023
+    # i_slice_s = 52  # R=0.829, s=2.023 ???
+    # i_slice_s = 70  # further out than strikepoint
+    i_slice_s = 60  # further out than strikepoint
+    s_slice = s_path[i_slice_s]
+    r_slice = r_path[i_slice_s] if (r_path is not None) else None
+
+    # Radius of slice
+    fig, axes, ax_passed = plot_tools.get_fig_ax(num=f'Radius of slice-{machine}-{diag_tag_analysed}-{pulse}-R_{r_slice:0.3g}',
+                                                 ax_grid_dims=(1, 1), sharex=False, axes_flatten=False)
+    ax = axes
+    ax.imshow(list(data.values())[0], origin='lower', cmap='coolwarm')
+    ax.axvline(i_slice_s, alpha=0.7, color='red', ls='--', label=f's={s_slice} m, R={r_slice:0.3g} m')
+    plot_tools.annotate_providence(ax, meta_data=meta)
+    plot_tools.legend(ax=ax)
+    plot_tools.save_fig(base_paths['fire_user_dir'] / 'figures/alpha_scan/temporal_profile/' /
+                        f'heatmap_r_slice-{machine}-{diag_tag_analysed}-{pulse}-R_{r_slice:0.3g}.png', mkdir_depth=3)
+    plot_tools.show_if(show=True, tight_layout=True)
+
+    # Temporal profile at fixed radius
+    fig, axes, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_temporal-{machine}-{diag_tag_analysed}-{pulse}-R_{r_slice:0.3g}',
+                                                 ax_grid_dims=(1, 1), sharex=False, axes_flatten=False)
+    ax = axes
+    ax.axhline(0, alpha=0.7, color='k', ls='--')
+    for alpha, heat_map in reversed(list(data.items())):
+        alpha = alpha[0]
+        profile_t = heat_map[:, i_slice_s]
+        ax.plot(t, profile_t, marker='.', color=None, alpha=0.6, lw=1, markersize=1,
+                label=r'$q_{\perp}$'+fr'(R={r_slice:0.3g}), $\alpha$=' + fr'{alpha:0.3g}')
+    ax.set_xlabel('$t$ [s]')
+    ax.set_ylabel('$q_{\perp}$ [MW/m$^2$]')
+    plot_tools.annotate_providence(ax, meta_data=meta)
+    plot_tools.legend(ax=ax)
+    plot_tools.save_fig(base_paths['fire_user_dir'] / 'figures/alpha_scan/temporal_profile/' /
+                                     f'alpha_scan_q_stats-{machine}-{diag_tag_analysed}-{pulse}-R_{r_slice:0.3g}.png', mkdir_depth=3)
+    plot_tools.show_if(show=True, tight_layout=True)
+
+    # Heatmaps
+    fig, axes, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_heatmaps-{machine}-{diag_tag_raw}-{pulse}',
+                                                 ax_grid_dims=(2, int(np.ceil(len(alpha_values)/2))),
+                                                 figsize=(12, 12))
+    axes = axes.flatten()
+    for i, alpha in enumerate(alpha_values):
         ax = axes[i]
+        heat_flux = data[tuple(alpha)]
         im = ax.pcolor(heat_flux, cmap='coolwarm')
         plot_tools.annotate_axis(ax, rf'$\alpha=${alpha}', loc='top left')
         plt.colorbar(im, ax=ax)
-        plt.tight_layout()
-    ax.axvline(x=alpha_passed, ls='--', color='k')
+        # plt.tight_layout()
+    if alpha_passed is not None:
+        ax.axvline(x=alpha_passed, ls='--', color='k')
     plot_tools.annotate_providence(ax, meta_data=meta)
-    plot_tools.save_fig(base_paths / 'figures/alpha_scan/heatmaps/'
+    plot_tools.save_fig(base_paths['fire_user_dir'] / 'figures/alpha_scan/heatmaps/' /
                                 f'alpha_scan_heatmaps-{machine}-{diag_tag_analysed}-{pulse}.png', mkdir_depth=3)
     plot_tools.show_if(True)
 
-    fig, ax, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_q_stats, {machine} {diag_tag_analysed} {pulse}',
+    # Plot stats
+    fig, ax, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_q_stats-{machine}-{diag_tag_analysed}-{pulse}',
                                                ax_grid_dims=(1, 1))
     alphas_flat = [a[0] for a in alpha_values]
-    for stat, values in stats.items():
+    for stat, values in stats_dict.items():
         ax.plot(alphas_flat, values, marker='x', color=None, label=stat)
     # ax.plot(alphas_flat, medians, marker='.', color=None, label='median')
     # ax.plot(alphas_flat, maxs, marker='o', color=None, label='max')
@@ -98,11 +159,11 @@ def scan_alpha_param(temperature_path, t, s_path, theo_kwargs, alpha_values, tes
     plot_tools.legend(ax)
     ax.set_xlabel(r'$\alpha$ [W/(m$^2$•K)]')
     ax.set_ylabel(r'$q_\perp$ [MW]')
-    plot_tools.save_fig(base_paths / 'figures/alpha_scan/q_stats/'
-                                     'alpha_scan_q_stats-{machine}-{diag_tag_analysed}-{pulse}.png', mkdir_depth=2)
+    plot_tools.save_fig(base_paths['fire_user_dir'] / 'figures/alpha_scan/q_stats/' /
+                                     f'alpha_scan_q_stats-{machine}-{diag_tag_analysed}-{pulse}.png', mkdir_depth=3)
     plot_tools.show_if(show=True, tight_layout=True)
 
-    fig, axes, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_radial_av, {machine} {diag_tag_analysed} {pulse}',
+    fig, axes, ax_passed = plot_tools.get_fig_ax(num=f'alpha_scan_radial_av-{machine}-{diag_tag_analysed}-{pulse}',
                                                ax_grid_dims=(1, 2), sharex=True)
     ax = axes[0]
     for alpha, profile in zip(alphas_flat, radial_average['heat_flux']):
@@ -113,8 +174,8 @@ def scan_alpha_param(temperature_path, t, s_path, theo_kwargs, alpha_values, tes
 
     ax = axes[1]
     ax.plot(t, radial_average['temperature_max'][0], marker='.', ls='-', color=None, label=r'$T_{max}$')
-    ax.plot(t, radial_average['temperature_98%'][0], marker='.', ls='-', color=None, label=r'$T_{98%}$')
-    ax.plot(t, radial_average['temperature_95%'][0], marker='.', ls='-', color=None, label=r'$T_{95%}$')
+    ax.plot(t, radial_average['temperature_98%'][0], marker='.', ls='-', color=None, label=r'$T_{98}$')
+    ax.plot(t, radial_average['temperature_95%'][0], marker='.', ls='-', color=None, label=r'$T_{95}$')
     ax.plot(t, radial_average['temperature_av'][0], marker='.', ls='-', color='k', label=r'$T_{av}$')
     ax.plot(t, radial_average['temperature_min'][0], marker='.', ls='-', color=None, label=r'$T_{min}$')
 
@@ -122,9 +183,11 @@ def scan_alpha_param(temperature_path, t, s_path, theo_kwargs, alpha_values, tes
 
     ax.set_xlabel('$t$ [s]')
     ax.set_ylabel('$T$ [$^\circ$C]')
-    plot_tools.save_fig(base_paths / 'figures/alpha_scan/radial_av/'
-                                      f'alpha_scan_radial_av-{machine}-{diag_tag_analysed}-{pulse}.png',mkdir_depth=2)
+    plot_tools.save_fig(base_paths['fire_user_dir'] / 'figures/alpha_scan/radial_av/' /
+                        f'alpha_scan_radial_av-{machine}-{diag_tag_analysed}-{pulse}.png', fig=fig, mkdir_depth=2)
     plot_tools.show_if(show=True, tight_layout=True)
+
+    pass
 
 
 def calc_heatflux(t, temperatures, path_data, path_name, theo_kwargs, force_material_sub_index=None, meta=()):
@@ -143,7 +206,6 @@ def calc_heatflux(t, temperatures, path_data, path_name, theo_kwargs, force_mate
     Returns: heat_flux_2d(s_path, t), extra_results
 
     """
-    from fire import theodor
     meta = dict(meta)
 
     t = np.array(t)
@@ -245,10 +307,40 @@ def calc_heatflux(t, temperatures, path_data, path_name, theo_kwargs, force_mate
     scan_alphas = False
     # scan_alphas = True
     if scan_alphas:
+        r_path = np.array(path_data[f'R_{path}'])
         # alpha_values = [[70000], [10000]]
-        alpha_values = [[a] for a in np.arange(10e2, 250e3, 25e3)]
+        # alpha_values = [[a] for a in np.arange(10e2, 250e3, 25e3)]
+        # alpha_values = [[a] for a in np.arange(10e2, 250e3, 50e3)]
+        # alpha_values = [[a] for a in np.logspace(3, 5, 3)]
+        # alpha_values = [[a] for a in [2e3, 5e3, 1e4, 3e4, 7e4, 1.5e5, 4e5]]
+        # alpha_values = [[a] for a in [1e4, 3.5e4, 7e4, 1.4e5, 3e5]]
+        alpha_values = [[a] for a in [3e4, 5e4, 7e4, 9e4, 12e4, 30e4]]
         alpha_values = [np.array(a) for a in alpha_values]
-        scan_alpha_param(temperature_path, t, s_path, theo_kwargs, alpha_values, test=True, meta=meta, verbose=True)
+
+        load_pickle_alpha_scan = True
+        # load = False
+
+        pulse, diag_tag_raw, machine = dict(meta).get('pulse'), dict(meta).get('diag_tag_raw'), dict(meta).get(
+            'machine')
+        diag_tag_analysed = uda_utils.get_analysed_diagnostic_tag(diag_tag_raw)
+        alpha_passed = theo_kwargs.get('alpha_top_org')
+
+        path_fn_pickle = Path(f'alpha_scan-{len(alpha_values)}_alphas-{machine}-{diag_tag_raw}-{pulse}.p')
+
+        if (not load_pickle_alpha_scan) or (not path_fn_pickle.is_file()):
+            data, stats_dict, radial_average = calc_alpha_scan(temperature_path, t, s_path, theo_kwargs,
+                        alpha_values, test=True, meta=meta, r_path=r_path, path_fn_pickle=path_fn_pickle, verbose=True)
+        else:
+            data = io_basic.pickle_load(path_fn_pickle)
+            data, stats_dict, radial_average, alpha_values_loaded = [data[key] for key in
+                                                         ('data', 'stats', 'radial_average', 'alpha_values')]
+            if not np.all(np.isclose(alpha_values, alpha_values_loaded)):
+                logger.warning('Loaded different alpha values for scan')
+                data, stats_dict, radial_average = calc_alpha_scan(temperature_path, t, s_path, theo_kwargs,
+                                                                   alpha_values, test=True, meta=meta, r_path=r_path,
+                                                                   path_fn_pickle=path_fn_pickle, verbose=True)
+        plot_alpha_scan(data, stats_dict, radial_average, alpha_values, t, s_path, r_path=r_path, meta=meta,
+                        alpha_passed=alpha_passed)
 
     # Convert W -> MW
     heat_flux *= 1e-6
